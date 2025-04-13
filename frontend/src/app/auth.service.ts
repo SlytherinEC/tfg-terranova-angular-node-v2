e4f077e93ee995ffe78976ce85bbc62c0cf81668
Yulian Borges Lobaina <yborges2005@gmail.com>
2025-04-13 19:20:28 +0200

    Agregar métodos de autenticación y gestión de sesión en AuthService. 'Tpgtahc'

--- frontend/src/app/auth.service.ts
+++ frontend/src/app/auth.service.ts
@@ -1,9 +1,57 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { firstValueFrom, Observable } from 'rxjs';
+import { Usuario } from './usuario';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  private apiUrl = 'http://localhost:4205/api/usuarios';
+
+  constructor(private http: HttpClient) { }
+
+  // Métodos para registrar un nuevo usuario
+  registro(usuario: Usuario): Observable<any> {
+    return this.http.post(`${this.apiUrl}/registro`, {
+      nombre: usuario.nombreUsuario,
+      email: usuario.email,
+      contrasena: usuario.contrasena
+    });
+  }
+
+  // Método para iniciar sesión
+  login(email: string, contrasena: string): Observable<any> {
+    return this.http.post(`${this.apiUrl}/login`, { email, contrasena });
+  }
+
+  // Método para verificar si el usuario está autenticado
+  estaAutenticado(): boolean {
+    return !!this.obtenerToken();
+  }
+
+  // Método para guardar el token en localStorage
+  guardarToken(token: string): void {
+    localStorage.setItem('token', token);
+  }
+
+  // Método para obtener el token
+  obtenerToken(): string | null {
+    return localStorage.getItem('token');
+  }
+
+  // Método para cerrar sesión. Elimina el token de localStorage
+  cerrarSesion(): void {
+    localStorage.removeItem('token');
+  }
+
+  // Método para obtener header con token JWT
+  obtenerHeadersAuth(): HttpHeaders {
+    const token = this.obtenerToken();
+    return new HttpHeaders({
+      Authorization: `Bearer ${token}`
+    })
+  }
+
 }
