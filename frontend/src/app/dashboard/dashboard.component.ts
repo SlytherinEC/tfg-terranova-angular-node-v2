24591b13e0364aecdd1bb87c8140d2240cb4364e
Yulian Borges Lobaina <yborges2005@gmail.com>
2025-04-14 01:46:42 +0200

    Agregar funcionalidad de cierre de sesión en el componente del dashboard

--- frontend/src/app/dashboard/dashboard.component.ts
+++ frontend/src/app/dashboard/dashboard.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { DiceComponent } from '../dice/dice.component';
+import { AuthService } from '../auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-dashboard',
@@ -9,5 +11,17 @@ import { DiceComponent } from '../dice/dice.component';
   styleUrl: './dashboard.component.scss'
 })
 export class DashboardComponent {
+
+  constructor(private authService: AuthService, private router: Router) { }
+
+  async logout(): Promise<void> {
+    try {
+      await this.authService.cerrarSesion();
+      this.router.navigate(['/login']);
+    } catch (error) {
+      console.error('Error al cerrar sesión:', error);
+      
+    }
+  }
 
 }
