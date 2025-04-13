04d2545b8dd7f163f72f00669b541e5b8368b361
Yulian Borges Lobaina <yborges2005@gmail.com>
2025-04-14 00:51:48 +0200

    Agregar configuración de rutas y proveedor de HttpClient en la aplicación

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,7 +1,13 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'dashboard', component: DashboardComponent },
 ];
