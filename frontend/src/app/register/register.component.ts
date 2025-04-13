148172350d1001a90d2030cf2a9711f68f1ee70b
Yulian Borges Lobaina <yborges2005@gmail.com>
2025-04-13 18:09:29 +0200

    Agregar formulario de registro con validaciones y estructura

--- frontend/src/app/register/register.component.ts
+++ frontend/src/app/register/register.component.ts
@@ -1,11 +1,30 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  standalone: true,
+  imports: [ReactiveFormsModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.scss'
 })
 export class RegisterComponent {
+  registerForm: FormGroup;
+  errorMessage: String | null = null;
+  successMessage: String | null = null;
+  loading: Boolean = false;
 
+  constructor(private fb: FormBuilder, private authService: AuthService) {
+    this.registerForm = this.fb.group({
+      username: ['', [Validators.required, Validators.minLength(3)]],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      confirmPassword: ['', [Validators.required, Validators.minLength(6)]]
+    });
+  }
+
+  async onSubmit() {
+    
+  }
 }
