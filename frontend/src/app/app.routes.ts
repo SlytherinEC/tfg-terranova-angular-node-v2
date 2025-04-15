be024ba8e010bc0a9d15693f2af6c651f508c08f
Yulian Borges Lobaina <yborges2005@gmail.com>
2025-04-15 02:44:21 +0200

    Agregar guardia de acceso público y aplicar protección a las rutas de inicio de sesión y registro

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -4,11 +4,12 @@ import { RegisterComponent } from './register/register.component';
 import { LoginComponent } from './login/login.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { authGuard } from './guards/auth.guard';
+import { publicGuard } from './guards/public.guard';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', component: HomeComponent },
-  { path: 'register', component: RegisterComponent },
-  { path: 'login', component: LoginComponent },
+  { path: 'home', canActivate: [publicGuard], component: HomeComponent },
+  { path: 'register', canActivate: [publicGuard], component: RegisterComponent },
+  { path: 'login', canActivate: [publicGuard], component: LoginComponent },
   { path: 'dashboard', canActivate: [authGuard], component: DashboardComponent },
 ];
--- frontend/src/app/guards/public.guard.ts
+++ frontend/src/app/guards/public.guard.ts
@@ -2,15 +2,14 @@ import { inject } from '@angular/core';
 import { CanActivateFn, Router } from '@angular/router';
 import { AuthService } from '../auth.service';
 
-export const authGuard: CanActivateFn = (route, state) => {
+export const publicGuard: CanActivateFn = (route, state) => {
 
   const authService = inject(AuthService);
   const router = inject(Router);
 
   if (authService.estaAutenticado()) {
-    return true;
-  } else {
-    router.navigate(['/login']);
+    router.navigate(['/dashboard']);
     return false;
   }
+  return true;
 };
