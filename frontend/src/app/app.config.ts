65e17fae75fe68c12ef3d230426d47b4df918c5e
Yulian Borges Lobaina <yborges2005@gmail.com>
2025-05-02 17:26:00 +0200

    Agregar interceptor de autenticación para manejar errores de autorización

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -3,13 +3,16 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideNoopAnimations } from '@angular/platform-browser/animations';
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { authInterceptor } from './interceptors/auth.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }), 
     provideRouter(routes),
     provideNoopAnimations(),
-    provideHttpClient()
+    provideHttpClient(
+      withInterceptors([authInterceptor]),
+    )
   ]
 };
--- frontend/src/app/interceptors/auth.interceptor.ts
+++ frontend/src/app/interceptors/auth.interceptor.ts
@@ -1,5 +1,22 @@
-import { HttpInterceptorFn } from '@angular/common/http';
+import { HttpErrorResponse, HttpRequest, HttpInterceptorFn } from "@angular/common/http";
+import { inject } from "@angular/core";
+import { catchError, throwError } from "rxjs";
+import { AuthService } from "../services/auth.service";
+import { Router } from "@angular/router";
 
-export const authInterceptor: HttpInterceptorFn = (req, next) => {
-  return next(req);
+export const authInterceptor: HttpInterceptorFn = (req: HttpRequest<any>, next) => {
+  const authService = inject(AuthService);
+  const router = inject(Router);
+
+  return next(req).pipe(
+    catchError((error: HttpErrorResponse) => {
+      if (error.status === 401 || error.status === 403) {
+        console.warn('[AuthInterceptor] Token inválido o sin permisos. Cerrando sesión...');
+        authService.cerrarSesion();
+        router.navigate(['/login']);
+      }
+
+      return throwError(() => error);
+    })
+  );
 };
