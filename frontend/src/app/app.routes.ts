160add7d883d1e40c806e06d885f22dd21445df1
Yulian Borges Lobaina <yborges2005@gmail.com>
2025-05-08 01:03:12 +0200

    Actualizar rutas para utilizar ListaUsuariosComponent en lugar de AdminComponent y agregar ruta para PerfilComponent

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -6,8 +6,9 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 import { authGuard } from './guards/auth.guard';
 import { publicGuard } from './guards/public.guard';
 import { adminGuard } from './guards/admin.guard';
-import { AdminComponent } from './admin/admin.component';
 import { NoAutorizadoComponent } from './no-autorizado/no-autorizado.component';
+import { PerfilComponent } from './perfil/perfil.component';
+import { ListaUsuariosComponent } from './lista-usuarios/lista-usuarios.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
@@ -15,6 +16,7 @@ export const routes: Routes = [
   { path: 'register', canActivate: [publicGuard], component: RegisterComponent },
   { path: 'login', canActivate: [publicGuard], component: LoginComponent },
   { path: 'dashboard', canActivate: [authGuard], component: DashboardComponent },
-  { path: 'admin', canActivate: [adminGuard], component: AdminComponent },
+  { path: 'admin', canActivate: [adminGuard], component: ListaUsuariosComponent },
   { path: 'no-autorizado', canActivate: [authGuard], component: NoAutorizadoComponent },
+  { path: 'perfil', canActivate: [authGuard], component: PerfilComponent },
 ];
