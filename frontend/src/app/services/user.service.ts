8508b41c250aeb68d128bf92f2bc5a3c3b8b01a5
Yulian Borges Lobaina <yborges2005@gmail.com>
2025-05-08 01:02:54 +0200

    Refactor UserService para centralizar la obtención de encabezados en un método privado

--- frontend/src/app/services/user.service.ts
+++ frontend/src/app/services/user.service.ts
@@ -12,19 +12,29 @@ export class UserService {
 
   constructor(private http: HttpClient, private authService: AuthService) { }
 
-  obtenerUsuarioPorId(id: number): Observable<any> {
-    const headers = new HttpHeaders({
+  private getHeaders(): HttpHeaders {
+    return new HttpHeaders({
       Authorization: `Bearer ${this.authService.obtenerToken()}`
     });
+  }
 
-    return this.http.get<any>(`${this.apiUrl}/${id}`, { headers });
+  obtenerUsuarioPorId(id: number): Observable<any> {
+    return this.http.get<any>(`${this.apiUrl}/${id}`, { headers: this.getHeaders() });
   }
 
   obtenerPerfil(): Observable<any> {
-    const headers = new HttpHeaders({
-      Authorization: `Bearer ${this.authService.obtenerToken()}`
-    });
-    return this.http.get<any>(`${this.apiUrl}/perfil`, { headers });
+    return this.http.get<any>(`${this.apiUrl}/perfil`, { headers: this.getHeaders() });
   }
 
+  actualizarPerfil(datos: any): Observable<any> {
+    return this.http.put<any>(`${this.apiUrl}/perfil`, datos, { headers: this.getHeaders() });
+  }
+
+  cambiarContrasena(contrasenaActual: string, nuevaContrasena: string): Observable<any> {
+    const datos = {
+      contrasenaActual,
+      nuevaContrasena
+    };
+    return this.http.patch<any>(`${this.apiUrl}/cambiar-contrasena`, datos, { headers: this.getHeaders() });
+  }
 }
