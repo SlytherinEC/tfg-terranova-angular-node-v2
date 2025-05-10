3f1fd62d543daa197e8440601961d23977f39f61
Yulian Borges Lobaina <yborges2005@gmail.com>
2025-05-10 16:26:17 +0200

    Agregar componente GameMenu con su HTML, CSS y pruebas unitarias

--- frontend/src/app/game/game-menu/game-menu.component.ts
+++ frontend/src/app/game/game-menu/game-menu.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { GameService } from '../../../services/game.service';
+import { GameService } from '../../services/game.service';
 import { Router } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
