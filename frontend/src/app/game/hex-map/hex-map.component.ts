09e060a03b099cbc9c91c9360c6888eade6cf48b
Yulian Borges Lobaina <yborges2005@gmail.com>
2025-05-12 02:24:31 +0200

    Añadir componente HexMap con su HTML, CSS y pruebas unitarias

---
+++ frontend/src/app/game/hex-map/hex-map.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-hex-map',
+  imports: [],
+  templateUrl: './hex-map.component.html',
+  styleUrl: './hex-map.component.scss'
+})
+export class HexMapComponent {
+
+}
