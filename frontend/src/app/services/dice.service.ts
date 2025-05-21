49f92ec0a3ca8b2f9ea24ec72a429148fe583f85
Yulian Borges Lobaina <yborges2005@gmail.com>
2025-05-21 02:31:20 +0200

    Agregar servicio DiceService con métodos para lanzar dados genéricos, para exploración, combate y eventos

---
+++ frontend/src/app/services/dice.service.ts
@@ -0,0 +1,57 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { AuthService } from './auth.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DiceService {
+  private apiUrl = 'http://localhost:4205/api/dice';
+
+  constructor(
+    private http: HttpClient,
+    private authService: AuthService
+  ) { }
+
+  // Lanzar dados genéricos
+  rollDice(count: number = 1, sides: number = 6): Observable<any> {
+    return this.http.post(`${this.apiUrl}/roll`, { count, sides }, {
+      headers: this.authService.obtenerHeadersAuth()
+    });
+  }
+
+  // Lanzar dados para exploración
+  rollForExploration(idPartida: number): Observable<any> {
+    return this.http.post(`${this.apiUrl}/roll-for-action`, {
+      id_partida: idPartida,
+      accion: 'exploracion'
+    }, {
+      headers: this.authService.obtenerHeadersAuth()
+    });
+  }
+
+  // Lanzar dados para combate
+  rollForCombat(idPartida: number, arma: string, bonusPrecision: number = 0): Observable<any> {
+    return this.http.post(`${this.apiUrl}/roll-for-action`, {
+      id_partida: idPartida,
+      accion: 'combate',
+      parametros: {
+        arma,
+        bonusPrecision
+      }
+    }, {
+      headers: this.authService.obtenerHeadersAuth()
+    });
+  }
+
+  // Lanzar dados para eventos
+  rollForEvent(idPartida: number): Observable<any> {
+    return this.http.post(`${this.apiUrl}/roll-for-action`, {
+      id_partida: idPartida,
+      accion: 'evento'
+    }, {
+      headers: this.authService.obtenerHeadersAuth()
+    });
+  }
+}
